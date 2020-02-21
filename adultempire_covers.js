fba067b5988bd853f26b4dbc67dde7d6ef4fefab
boi123212321 <46821810+boi123212321@users.noreply.github.com>
2020-02-21 17:23:38 +0100

    Create adultempire_covers.js

---
+++ adultempire_covers.js
@@ -0,0 +1,62 @@
+/**
+ * NOTE this won't work with 0.13.1 or below
+ */
+async ({
+  args,
+  event,
+  $axios,
+  $cheerio,
+  $throw,
+  $log,
+  movieName,
+  $createImage
+}) => {
+  if (event != "movieCreated")
+    $throw("Uh oh. You shouldn't use the plugin for this type of event");
+
+  $log(
+    `Scraping movie covers for '${movieName}', dry mode: ${args.dry ||
+      false}...`
+  );
+
+  const url = `https://www.adultempire.com/allsearch/search?q=${movieName}`;
+  const html = (await $axios.get(url)).data;
+  const $ = $cheerio.load(html);
+
+  const firstResult = $(".boxcover").toArray()[0];
+  const href = $(firstResult).attr("href");
+
+  if (href) {
+    const movieUrl = "https://adultempire.com" + href;
+    const html = (await $axios.get(movieUrl)).data;
+    const $ = $cheerio.load(html);
+
+    const frontCover = $("#front-cover img").toArray()[0];
+    const frontCoverSrc = $(frontCover).attr("src");
+    const backCoverSrc = frontCoverSrc.replace("h.jpg", "bh.jpg");
+
+    if (args.dry === true) {
+      $log({
+        movieUrl,
+        frontCoverSrc,
+        backCoverSrc
+      });
+    } else {
+      const frontCoverImg = await $createImage(
+        frontCoverSrc,
+        `${movieName} (front cover)`
+      );
+      const backCoverImg = await $createImage(
+        backCoverSrc,
+        `${movieName} (back cover)`
+      );
+
+      return {
+        frontCover: frontCoverImg,
+        backCover: backCoverImg
+      };
+    }
+  }
+
+  return {};
+};
